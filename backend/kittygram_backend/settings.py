18a23ff11a45eb94e14f406d54fd3bc6ca658f3b
Roman Inozemtsev <dev@koyote92.ru>
2023-06-11 23:56:49 +0500

    first

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -59,8 +59,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django'),
+        'HOST': os.getenv('DB_HOST', 'postgres'),
         'PORT': os.getenv('DB_PORT', 5432),
     }
 }
